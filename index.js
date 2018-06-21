11f0f0943feaf7cd71e13478388382fd7cc13db4
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-06-21 13:47:11 -0300

    :tada: start

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').load();
+
+
+const app = require('http').createServer((req, res) => res.send('Ahoy!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
